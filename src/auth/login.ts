0a23477cae2c393e778503289d438543f8c4c091
Samuele Besoli <samuele.besoli.sb@gmail.com>
2023-06-02 17:32:52 +0200

    hash password before save

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -3,16 +3,20 @@ import { AuthForm, AuthResponse } from '../types/auth'
 import dotenv from 'dotenv'
 import { GraphQLError } from 'graphql'
 import { poolPostgres } from '../services/databases.js'
+import crypto from 'crypto'
 
 dotenv.config({ path: '.env.dev' })
 const SECRET_KEY = process.env.SECRET_KEY
 
 export async function login(form: AuthForm): Promise<AuthResponse> {
-    const { username, password } = form
+    const password = crypto
+        .createHash('md5')
+        .update(form.password)
+        .digest('hex')
 
     const query = await poolPostgres.query(
         'SELECT * FROM public.users WHERE username = $1 AND password_md5 = $2',
-        [username, password]
+        [form.username, password]
     )
     if (query.rows.length === 0) {
         throw new GraphQLError(
@@ -28,7 +32,7 @@ export async function login(form: AuthForm): Promise<AuthResponse> {
     const customer_id = query.rows[0].customer_id
     return {
         token: jwt.sign(
-            { username: username, customer_id: customer_id },
+            { username: form.username, customer_id: customer_id },
             SECRET_KEY,
             {
                 expiresIn: '24h',
